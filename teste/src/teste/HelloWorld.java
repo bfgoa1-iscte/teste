987251710bf4d32eac15e7b7321f0757b0276117
bfgoa1-iscte <bfgoa1@iscte-iul.pt>
2022-02-22 21:48:39 +0000

    conflito

--- teste/src/teste/HelloWorld.java
+++ teste/src/teste/HelloWorld.java
@@ -4,6 +4,6 @@ package teste;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello World."); 
     }
 }
